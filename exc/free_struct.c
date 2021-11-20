3b29fa4b293ab153fff9fa78abd7d3f392f5cc19
Mustapha Ouassit <mouassit@e1r13p12.1337.ma>
2021-11-20 11:50:51 +0100

    remove exec

--- exc/free_struct.c
+++
@@ -1,8 +0,0 @@
-# include "minishell.h"
-
-void    free_envp(t_envp *env)
-{
-    free(env->key);
-    free(env->value);
-    free(env);
-}
--- exc/ft_pwd.c
+++
@@ -1,9 +0,0 @@
-# include "minishell.h"
-
-void	ft_pwd()
-{
-	char buf[100];
-
-	printf("%s\n",getcwd(buf, sizeof(buf)));
-
-}
