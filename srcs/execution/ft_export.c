863f6a4b40cbcb5a356a63b95c980ae32b99e7da
Ayoub Afdel <ayafdel@e1r8p12.1337.ma>
2021-12-13 11:50:50 +0100

    working export a+=

--- srcs/execution/ft_export.c
+++ srcs/execution/ft_export.c
@@ -21,17 +21,26 @@ void print_exp(t_envp *head) {
     }
 }
 
+int		init_envp_node(t_envp **node)
+{
+	*node = malloc(sizeof(t_envp));
+	(*node)->value = 0;
+	(*node)->key = 0;
+	(*node)->plus = 0;
+	(*node)->equal = 0;
+	return (0);
+}
 t_envp*	fill_envp(char *str)
 {
 	t_envp *node;
 
-	node = malloc(sizeof(t_envp));
-
+	init_envp_node(&node);
 	if (ft_strchr(str, '='))
 	{
 		node->equal = 1;
 		if (ft_strchr(str, '=') != str && *(ft_strchr(str, '=') - 1) == '+')
 		{
+			printf("HNAYA\n");
 			node->key = ft_substr(str, 0, ft_indexof(str, '+'));
 			node->plus = 1;
 		}
@@ -47,6 +56,7 @@ t_envp*	fill_envp(char *str)
 		node->equal = 0;
 		node->key = ft_strdup(str);
 		node->value = 0;
+		node->plus = 0;
 	}
 	node->next=NULL;
 	return node;
@@ -71,6 +81,30 @@ int     env_key_error(char *var)
     return(0);
 }
 
+void	env_replace(t_envp **tmp1, t_envp **node1)
+{
+	t_envp *tmp;
+	t_envp *node;
+
+	tmp = *tmp1;
+	node = *node1;
+	if (node->equal && node->plus)
+	{
+		printf("HAHOWA\n");
+		tmp->equal = 1;
+		if (node->value)
+		{
+			tmp->value = ft_free_first(tmp->value, ft_strjoin(tmp->value, node->value));
+		}
+	}
+	else if (node->equal)
+	{
+		tmp->equal = 1;
+		tmp->value = ft_free_first(tmp->value, ft_strdup_null(node->value));
+	}
+	free_envp(node);	
+}
+
 void	add_to_env(t_envp **head, t_envp *node)
 {
 	t_envp *tmp;
@@ -85,21 +119,8 @@ void	add_to_env(t_envp **head, t_envp *node)
 	{
 		if (!ft_strcmp(node->key, tmp->key))
 		{
-			if (node->equal && node->plus)
-			{
-				printf("WORKING\n");
-				tmp->equal = 1;
-				if (node->value)
-					tmp->value = ft_free_first(tmp->value, ft_strjoin(tmp->value, node->value));
-				else
-					tmp->value = ft_free_first(tmp->value, ft_strdup(node->value));
-			}
-			else if (node->equal)
-			{
-				tmp->equal = 1;
-				tmp->value = ft_free_first(tmp->value, ft_strdup_null(node->value));
-			}
-			free_envp(node);
+			env_replace(&tmp, &node);
+			//free_envp(node);
 			return ;
 		}
 		if (tmp->next == NULL)
